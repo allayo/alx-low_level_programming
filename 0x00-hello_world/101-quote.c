0a25fbfadd30c6feaa43fbae50c12ce8a0d6823c
Ayo <ayo@gmail.com>
2022-12-08 15:57:01 -0800

    Task 8 c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
- * @void: parameter
- * Return: 1 (success)
+ * Return: 1
  */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful
-	\" - \ Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
