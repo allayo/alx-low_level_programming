53e8a48c8cdb7ae6b698365258052b9c7a69ddcd
Ayo <ayo@gmail.com>
2023-01-12 14:23:53 -0800

    task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -119,8 +119,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	for (l1 = 0; argv[1][l1]; l1++);
-	for (l2 = 0; argv[2][l2]; l2++);
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
 	ln = l1 + l2 + 1;
 	a = malloc(ln * sizeof(char));
 	if (a == NULL)
