d6f09808d70d915318885aefa37f91042b24b6bf
Ayo <ayo@gmail.com>
2023-01-12 14:20:39 -0800

    task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,7 +3,7 @@
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated
- * @ptr: pointer to the memory previously allocated
+ * @old_size:is the size, in bytes, of the allocated space for ptr
  * @new_size: the new size, in bytes of the new memory block
  * Return: pointer allocate new size memory, or NULL
  */
