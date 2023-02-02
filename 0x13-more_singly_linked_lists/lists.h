997733ae465735d4e005ddb645dcfbc148dc4278
Ayo <ayo@gmail.com>
2023-02-01 21:09:12 -0800

    Uppdate task 13

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,7 +14,8 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}
+listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -33,4 +34,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
 
-#endif /*LISTS_H*/
+#endif
