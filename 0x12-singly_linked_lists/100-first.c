209e7b444a73a4f7e8493a18070183a27d0ff096
Ayo <ayo@gmail.com>
2023-02-01 23:00:57 -0800

    Task 12 Updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,13 +4,11 @@
  * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
  * is executed before main()
  */
-
 void myStartupFun(void) __attribute__ ((constructor));
 
 /**
  * myStartupFun - implementation of myStartupFun
  */
-
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
